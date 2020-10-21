97f7ee1cb13570d827f9f38e79ca02a415c20c72
Roni Gabbay <ronigabbay8@gmail.com>
2020-10-21 14:52:43 +0300

    canvad and input

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,1 +1,20 @@
 'use strict'
+
+
+function onInit() {
+    gCanvas = document.querySelector('#my-canvas');
+    gCtx = gCanvas.getContext('2d');
+    drawImg()
+}
+
+
+
+function onInput() {
+    var elTxtInput = document.querySelector('.text-line');
+    return elTxtInput.value;
+}
+
+setTimeout(function () {
+    drawText(onInput(), 250, 60)
+}, 5000)
+
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,1 +1,39 @@
 'use strict'
+
+var gCtx;
+var gCanvas;
+
+var gMeme = {
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I love football',
+            size: 34,
+            align: 'center',
+            color: 'red'
+        }
+    ]
+}
+
+
+function drawText(text, x, y) {
+    gCtx.strokeStyle = gMeme.lines.color;
+    // console.log(gMeme.lines[gMeme.selectedLineIdx].color);
+    gCtx.fillStyle = 'white'
+    gCtx.lineWidth = '2'
+    gCtx.font = `${gMeme.lines[gMeme.selectedLineIdx].size}px IMPACT`
+    gCtx.textAlign = `${gMeme.lines[gMeme.selectedLineIdx].align}`
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
+}
+
+
+
+function drawImg() {
+    var img = new Image();
+    img.src = `./imgs/meme-imgs (square)/${gMeme.selectedImgId}.jpg`;
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+    }
+}
